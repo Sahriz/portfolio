83d62a44867145f8482c91c7cf8098819a885468
Sahriz <johnthaiy@gmail.com>
2025-02-03 23:04:15 +0100

    Fixed projects
    Added some of my projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,14 +6,14 @@ const projects = [
       link: "https://github.com/Sahriz/InfiniteWorld"
     },
     {
-      title: "Ray Tracer",
-      description: "A ray tracing renderer written in C++ using the CPU for a course at LIU called TNCG15.",
+      title: "Pathtracer",
+      description: "A Pathtracing renderer written in C++ using the CPU for a course at LIU called TNCG15.",
       image: "/1600x1600RayTracer.png",
-      link: "https://github.com/yourgithub/raytracer"
+      link: "https://github.com/eLdOchLagor/TNCG15-Monte-Carlo-Pathtracer"
     },
     {
       title: "Solar system simulation",
-      description: "A Blender Python addon that simulates and animates the solar system, and adds materials textures to the planets.",
+      description: "A Blender Python addon that simulates and animates the solar system, and adds materials to the planets.",
       image: "/RedoVisning4.png",
       link: "https://github.com/Sahriz/BlenderSolarsystemSim?tab=readme-ov-file"
     }
