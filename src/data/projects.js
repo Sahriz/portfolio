20284f51e68c4a197f0f9caf45b26ea174ea0bff
Sahriz <johnthaiy@gmail.com>
2025-02-03 22:45:37 +0100

    Fixed most css elements on startpage

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,15 +1,21 @@
 const projects = [
     {
-      title: "Graphics Engine",
-      description: "A real-time rendering engine using C++ and OpenGL.",
-      //image: "/graphics-engine.png",
-      link: "https://github.com/yourgithub/graphics-engine"
+      title: "Procedurally Generated Terrain",
+      description: "A real-time infinite procedurally generated world using perlin noise in Unity.",
+      image: "/PlayerCharacterView.png",
+      link: "https://github.com/Sahriz/InfiniteWorld"
     },
     {
       title: "Ray Tracer",
       description: "A ray tracing renderer written in C++ using the CPU for a course at LIU called TNCG15.",
       image: "/1600x1600RayTracer.png",
       link: "https://github.com/yourgithub/raytracer"
+    },
+    {
+      title: "Solar system simulation",
+      description: "A Blender Python addon that simulates and animates the solar system, and adds materials textures to the planets.",
+      image: "/RedoVisning4.png",
+      link: "https://github.com/Sahriz/BlenderSolarsystemSim?tab=readme-ov-file"
     }
   ];
   export default projects;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useFrame } from '@react-three/fiber';
 import ShaderBanner from '../components/ShaderBanner';
 import projects from '../data/projects';
 
 export default function Portfolio() {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isInitialized, setIsInitialized] = useState(false);
+  const [canvasZIndex, setCanvasZIndex] = useState(9);
   useEffect(() => {
     const handleScroll = () => {
       const canvas = document.querySelector('.canvas-container');
       const overlayText = document.querySelector('.overlayText');
       const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
       const maxScroll = 300; // Adjust as needed for scroll-based fade-out
-  
+
       // Calculate opacity for scroll (both canvas and text fade out together)
       const opacity = Math.max(0, 1 - scrollTop / maxScroll);
-  
+
       // Apply opacity changes for canvas and overlayText (fade in/out with scroll)
       if (canvas) canvas.style.opacity = opacity;
       if (overlayText) {
@@ -22,9 +23,15 @@ export default function Portfolio() {
         overlayText.style.transition = "none";
         overlayText.style.opacity = opacity;
       }
+
+      // Change z-index when opacity is zero
+      if (opacity === 0) {
+        setCanvasZIndex(-1);
+      } else {
+        setCanvasZIndex(9);
+      }
     };
-    
-  
+
     // Fade-in the overlay text after 5 seconds
     const fadeInTimeout = setTimeout(() => {
       const overlayText = document.querySelector('.overlayText');
@@ -33,34 +40,54 @@ export default function Portfolio() {
         overlayText.style.opacity = 1; // Set opacity to 1 after 5 seconds
       }
     }, 5000); // 5 seconds delay for the fade-in
-  
+
     // Add scroll event listener
     window.addEventListener('scroll', handleScroll);
-  
+
     // Clean up the timeout and event listener on component unmount
     return () => {
       clearTimeout(fadeInTimeout);
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
+
+  
+
+  const MeshComponent = () => {
+    useFrame(() => {
+      if (!isInitialized) {
+        setIsInitialized(true);
+      }
+    });
+
+    return (
+      <mesh>
+        <planeGeometry args={[25, 25, 1250, 1250]} />
+        <ShaderBanner />
+      </mesh>
+    );
+  };
   
   return (
     <div className="relative w-full min-h-screen bg-black text-white">
       
       {/* Apply the CSS class to control size */}
-      <div className="canvas-container">
+      <div className="canvas-container" style={{ zIndex: canvasZIndex }}>
+      
       <Canvas
-  gl={{ alpha: false, antialias: true }}
-  style={{ width: "100%", height: "100%", position: "absolute", top: 0, left: 0 }}
-  camera={{ position: [0, 0, 10], fov: 75 }} // Adjust camera if needed
->
-          <mesh>
-            <planeGeometry args={[25, 25, 1500, 1500]} />
-            <ShaderBanner />
-          </mesh>
+          gl={{ alpha: false, antialias: true }}
+          style={{ width: "100%", height: "100%", position: "absolute", top: 0, left: 0 }}
+          camera={{ position: [0, 0, 10], fov: 75 }} // Adjust camera if needed
+        >
+          
+          <MeshComponent />
+            
+          
         </Canvas>
+     
       </div>
-
+      {isInitialized && (
+        <>
        {/* Overlay Text */}
        <div className="overlayText">
         <h1 className="title" style={{color: 'white'}}>Jonatan Ebenholms Portfolio</h1>
@@ -69,7 +96,7 @@ export default function Portfolio() {
 
       {/* Projects grid */}
       <div className='padder'>
-        <div className="projectContainer projectsGrid">
+        <div className="projectContainer projectsGrid move-down">
           {projects.map((project, index) => (
             <a key={index} href={project.link} className="projectLink">
               <img src={project.image} alt={project.title} className="projectImage" />
@@ -79,6 +106,8 @@ export default function Portfolio() {
           ))}
         </div>
       </div>
+      </>
+      )}
     </div>
   );
 }
