fe24fd9e898d9bf21e4237a800f1fae2dd1145f7
Ebenholm <johnthaiy@gmail.com>
2025-09-09 14:40:40 +0200

    Fix

--- src/pages/projects/TerrainLibrary.js
+++ src/pages/projects/TerrainLibrary.js
@@ -5,7 +5,7 @@ import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import rehypeRaw from 'rehype-raw';
 
-export default function TerrainLib() {
+export default function TerrainLibrary() {
   const [content, setContent] = useState('');
 
   useEffect(() => {
