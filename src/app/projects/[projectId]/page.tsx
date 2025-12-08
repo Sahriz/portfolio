35746beec53b065e0d1fa6da026e305620544b58
Ebenholm <johnthaiy@gmail.com>
2025-12-08 18:48:32 +0100

    website active

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useEffect, useState, use } from 'react';
+import { useEffect, useState, use } from 'react';
 import Link from 'next/link';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
