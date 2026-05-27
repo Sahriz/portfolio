3ee6e35ea105bf09bc9091f1f5addd4e33e61a88
Sahriz <94785672+Sahriz@users.noreply.github.com>
2026-05-28 01:58:04 +0200

    Added an animation to project cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -162,7 +162,7 @@ export default function Portfolio() {
       </div>
 
       <>
-        <section id="scroll-target-projects" className="relative z-10 mt-14">
+        <section id="scroll-target-projects" className="relative z-10 mt-[25vh]">
             <div className="mx-auto max-w-6xl px-4 sm:px-6 lg:px-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
               {projects.map((project) => (
                 <ProjectCard key={project.id} project={project} />
